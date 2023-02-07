5602d1579a462bd85c4d3200a6d910af9da99b5f
Romeu maier <romeumaier@gmail.com>
2023-02-07 15:02:13 -0300

    7fev

--- design/script.js
+++ design/script.js
@@ -1,29 +1,10 @@
-function trocaClasse(elemento, antiga, nova) {
-    elemento.classList.remove(antiga);
-    elemento.classList.add(nova);
-}
-
-setTimeout(function(){
-    var vrau = document.querySelector('vrau');
-    trocaClasse(vrau, 'Azul', 'verde');
-}, 357);
-
-
-let ratings = document.querySelectorAll('.circle');
-let btn = document.getElementsById('btn');
-let thankYou = document.querySelector('.thankYou');
-
-for (let i = 0;i < ratings.length; i++) {
-    let rating = ratings[i].innerHTML;
-
-    for (let i = 0; i < ratings.length; i++) {
-        ratings[i].classList.contains('selected')
+var btn = document.getElementsByClassName('btn');
+var escolha = document.querySelector('.evaluation');
+btn.addEventListener("click", function(){
+    if(escolha.getElementsByClassName.display === 'block'){
+        escolha.getElementsByClassName.display = 'none';
+    } else {
+        
     }
-}
 
-const getbtn = document.getElementsByClassName('.vrau');
-getbtn.addEventListener ("click", myfunction);
-
-function myfunction() {
-    document.getElementById("demo").innerHTML = "stay away";
-}
+};
